5cf95d262bd3052c31b04cc94b3fbb8a218ca2c5
Quentin <159776909+QuentinoDelFuego@users.noreply.github.com>
2024-04-12 00:23:17 +0200

    Update navbar.jsx

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
         <nav>
             <div className="flex flex-row" id="Navbar">
                     <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/aboutSection"}>About me</Link>
+                        <Link href={"https://strong-fairy-f76f3a.netlify.app/portfolioSections/aboutSection"}>About me</Link>
                     </button>
                     <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/ecoSection"}>Eco-friendly website</Link>
+                        <Link href={"https://strong-fairy-f76f3a.netlify.app/portfolioSections/ecoSection"}>Eco-friendly website</Link>
                     </button>
                     <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/projectsSection"}>My projects</Link>
+                        <Link href={"https://strong-fairy-f76f3a.netlify.app/portfolioSections/projectsSection"}>My projects</Link>
                     </button>
             </div>
         </nav>
