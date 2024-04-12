133d68c0c0a1607434e6f8c86266167032a52a3c
Quentin <159776909+QuentinoDelFuego@users.noreply.github.com>
2024-04-12 19:48:27 +0200

    forgot links

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <nav>
-            {/*<div className="flex flex-row" id="Navbar">
+            <div className="flex flex-row" id="Navbar">
                     <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
                         <Link href={"https://strong-fairy-f76f3a.netlify.app/portfolioSections/aboutSection"}>About me</Link>
                     </button>
@@ -13,17 +13,6 @@ const Navbar = () => {
                     </button>
                     <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
                         <Link href={"https://strong-fairy-f76f3a.netlify.app/portfolioSections/projectsSection"}>My projects</Link>
-                    </button>
-    </div>*/}
-            <div className="flex flex-row" id="Navbar">
-                    <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/aboutSection"}>About me</Link>
-                    </button>
-                    <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/ecoSection"}>Eco-friendly website</Link>
-                    </button>
-                    <button className="hover:text-amber-400 bg-white inline-block font-extrabold">
-                        <Link href={"http://localhost:3000/portfolioSections/projectsSection"}>My projects</Link>
                     </button>
             </div>
         </nav>
